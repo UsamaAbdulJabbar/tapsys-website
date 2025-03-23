3a8a6f32866129310cee0a87febdea4dfe21b20d
Usama Abdul Jabbar <usamabest4u@gmail.com>
2025-03-23 14:11:51 +0500

    reload issue update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+//   build: {
+//     outDir: "dist",
+//   },
+//   server: {
+//     historyApiFallback: true,  // Ye React Router k liye zaroori hai
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base:"/",
   plugins: [react()],
-  base: "./",
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
-    historyApiFallback: true,  // Ye React Router k liye zaroori hai
+    historyApiFallback: true,
   },
 });
